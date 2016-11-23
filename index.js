d80485d0964bf956bb8098180df81bea47679f65
Sintax1 <sintax@obscurepacket.org>
2016-11-23 19:18:02 +0000

    Updated test framework

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  /**
+  * Login to vSphere Server
+  */
+  authenticate: function(vSphereObj, user, pass) {
+    return true;
+  }
+
+}
--- test/nvec.js
+++ test/nvec.js
@@ -1,3 +1,10 @@
 var should = require('chai').should(),
-  nvec = require('../nvec');
+  vsphereCtrl = require('../index'),
+  authenticate = vsphereCtrl.authenticate;
+
+describe('#authenticate', function() {
+  it('Tests vSphere credentials', function() {
+    authenticate(null, null, null).should.equal(true);
+  });
+});
 
