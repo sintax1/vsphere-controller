3a00e50d53150b2d919b58b0958da84a965b8712
Sintax1 <sintax@obscurepacket.org>
2017-01-15 19:19:14 +0000

    Removed 'archive' directory of legacy files.

--- archive/public/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import './assets/css/common.css';
-import './assets/css/loginForms.css';
-import './assets/css/settingsForm.css';
-import './assets/css/starter.css';
-
-import './assets/css/angular-ui-switch.min.css';
-import './assets/css/font-awesome.css';
